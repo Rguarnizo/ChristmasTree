0ef4ad9edfab9a978a55bab4782594dd55d95d9c
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-17 18:32:31 -0500

    Example set clock of program

--- src/main.cpp
+++ src/main.cpp
@@ -1,141 +1,29 @@
-#include <Adafruit_NeoPixel.h>
+#include <Strip.h>
+#include <Arduino.h>
 
-#define PIXELSPIN   6
-#define NUMPIXELS   60
-#define CALIBRATIONTIME 20000
+ Strip strip = Strip(8,2);
+ int i = 0;
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(8, PIXELSPIN, NEO_GRB + NEO_KHZ800);
-
-unsigned long pixelsInterval=50;  // the time we need to wait
-unsigned long colorWipePreviousMillis=0;
-unsigned long theaterChasePreviousMillis=0;
-unsigned long theaterChaseRainbowPreviousMillis=0;
-unsigned long rainbowPreviousMillis=0;
-unsigned long rainbowCyclesPreviousMillis=0;
-
-int theaterChaseQ = 0;
-int theaterChaseRainbowQ = 0;
-int theaterChaseRainbowCycles = 0;
-int rainbowCycles = 0;
-int rainbowCycleCycles = 0;
-
-uint16_t currentPixel = 0;// what pixel are we operating on
-
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
-
-void colorWipe(uint32_t c){
-  pixels.setPixelColor(currentPixel,c);
-  pixels.show();
-  currentPixel++;
-  if(currentPixel == NUMPIXELS){
-    currentPixel = 0;
-  }
-}
-
-void rainbow() {
-  for(uint16_t i=0; i<pixels.numPixels(); i++) {
-    pixels.setPixelColor(i, Wheel((i+rainbowCycles) & 255));
-  }
-  pixels.show();
-  rainbowCycles++;
-  if(rainbowCycles >= 256) rainbowCycles = 0;
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle() {
-  uint16_t i;
-
-  //for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< pixels.numPixels(); i++) {
-      pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + rainbowCycleCycles) & 255));
-    }
-    pixels.show();
-
-  rainbowCycleCycles++;
-  if(rainbowCycleCycles >= 256*5) rainbowCycleCycles = 0;
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c) {
-  for (int i=0; i < pixels.numPixels(); i=i+3) {
-      pixels.setPixelColor(i+theaterChaseQ, c);    //turn every third pixel on
-    }
-    pixels.show();
-    for (int i=0; i < pixels.numPixels(); i=i+3) {
-      pixels.setPixelColor(i+theaterChaseQ, 0);        //turn every third pixel off
-    }
-    theaterChaseQ++;
-    if(theaterChaseQ >= 3) theaterChaseQ = 0;
+void setup(){   
+  Serial.begin(9600);
 }
 
 
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow() {
-  for (int i=0; i < pixels.numPixels(); i=i+3) {
-    pixels.setPixelColor(i+theaterChaseRainbowQ, Wheel( (i+theaterChaseRainbowCycles) % 255));    //turn every third pixel on
-  }
-      
-  pixels.show();
-  for (int i=0; i < pixels.numPixels(); i=i+3) {
-    pixels.setPixelColor(i+theaterChaseRainbowQ, 0);        //turn every third pixel off        
-  }      
-  theaterChaseRainbowQ++;
-  theaterChaseRainbowCycles++;
-  if(theaterChaseRainbowQ >= 3) theaterChaseRainbowQ = 0;
-  if(theaterChaseRainbowCycles >= 256) theaterChaseRainbowCycles = 0;
-}
+void loop(){
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-
-
-
-void setup() {
-  currentPixel = 0;
   
-  pixels.begin(); // This initializes the NeoPixel library.
-  pixels.show(); // This sends the updated pixel color to the hardware.
-      
-}
 
-void loop () {
+  if((unsigned long) (millis()-strip.getPreviousMillis()) >= strip.getTime()){
+    strip.setPreviousMillis(millis());    
+    Serial.println(strip.getPreviousMillis());
+    strip.strip.setPixelColor(i,strip.strip.Color(0,50,0));
+    strip.strip.show();
+    i++;
+    if(i == 7){
+      strip.strip.clear();
+      i = 0;
+    }
+  }   
+}
   
-        if ((unsigned long)(millis() - colorWipePreviousMillis) >= pixelsInterval) {
-          colorWipePreviousMillis = millis();
-          colorWipe(pixels.Color(0,255,125));
-        }
-      
-        if ((unsigned long)(millis() - theaterChasePreviousMillis) >= pixelsInterval) {
-          theaterChasePreviousMillis = millis();
-          theaterChase(pixels.Color(127, 127, 127)); // White
-        }
-      
-        if ((unsigned long)(millis() - theaterChaseRainbowPreviousMillis) >= pixelsInterval) {
-          theaterChaseRainbowPreviousMillis = millis();
-          theaterChaseRainbow();
-        }
-      
-        if ((unsigned long)(millis() - rainbowPreviousMillis) >= pixelsInterval) {
-          rainbowPreviousMillis = millis();
-          rainbow();
-        }
-      
-        if ((unsigned long)(millis() - rainbowCyclesPreviousMillis) >= pixelsInterval) {
-            rainbowCyclesPreviousMillis = millis();
-            rainbowCycle();
-        }  
 
-}
-
-// Fill the dots one after the other with a color
