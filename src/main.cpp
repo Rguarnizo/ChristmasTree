05902cf2c2fa3a14439ee0cce7cf8cabbbf905fe
rguarnizo <rguarnizo@unal.edu.co>
2021-02-19 13:44:10 -0500

    Pointer of array to KEEP ON Strip

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <Strip.h>
 #include <Arduino.h>
 
- Strip strip = Strip(8,2);
+ Strip* strips[1];
  int i = 0;
 
 void setup(){   
+  strips[0] = new Strip(8,2);
   Serial.begin(9600);
 }
 
@@ -13,14 +14,14 @@ void loop(){
 
   
 
-  if((unsigned long) (millis()-strip.getPreviousMillis()) >= strip.getTime()){
-    strip.setPreviousMillis(millis());    
-    Serial.println(strip.getPreviousMillis());
-    strip.strip.setPixelColor(i,strip.strip.Color(0,50,0));
-    strip.strip.show();
+  if((unsigned long) (millis()-strips[0]->getPreviousMillis()) >= strips[0]->getTime()){
+    strips[0]->setPreviousMillis(millis());    
+    Serial.println(strips[0]->getPreviousMillis());
+    strips[0]->strip.setPixelColor(i,strips[0]->strip.Color(0,50,0));
+    strips[0]->strip.show();
     i++;
     if(i == 7){
-      strip.strip.clear();
+      strips[0]->strip.clear();
       i = 0;
     }
   }   
